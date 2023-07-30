a30778397befb5dc4a7b01c54fd395be948a9494
Leonardo Silveira <sombriks@gmail.com>
2023-07-30 17:37:16 -0300

    cleanups. maven kotlin projects behaves slightly different from the gradle ones. yet got faster cold start builds and smaller memory footprint

--- service-consumer-2/src/main/kotlin/com/example/KafkaConsumerApplication.kt
+++ service-consumer-2/src/main/kotlin/com/example/KafkaConsumerApplication.kt
@@ -1,11 +1,11 @@
 package com.example
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KafkaConsumerApplication
 
 fun main(args: Array<String>) {
-	runApplication<KafkaConsumerApplication>(*args)
+	SpringApplication.run(KafkaConsumerApplication::class.java, *args)
 }
